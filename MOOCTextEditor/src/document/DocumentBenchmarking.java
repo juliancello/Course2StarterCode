85ed4d50e9aadb2250ff5d7f944cc28c80bd91ce
Christine Alvarado <cjalvarado@eng.ucsd.edu>
2015-10-03 21:04:01 -0700

    Rolled back the LaunchClass so that it uses the new dictionary interface.

--- MOOCTextEditor/src/document/DocumentBenchmarking.java
+++ MOOCTextEditor/src/document/DocumentBenchmarking.java
@@ -37,6 +37,29 @@ public class DocumentBenchmarking {
 		// TODO: Fill in the rest of this method so that it runs two loops
 		// and prints out timing results as described in the assignment 
 		// instructions.
+		for (int numToCheck = start; numToCheck < numSteps*increment + start; 
+				numToCheck += increment)
+		{
+			// numToCheck holds the number of characters that you should read from the 
+			// file to create both a BasicDocument and an EfficientDocument.  
+			
+			/* Each time through this loop you should:
+			 * 1. Print out numToCheck followed by a tab (\t) (NOT a newline)
+			 * 2. Read numToCheck characters from the file into a String
+			 *     Hint: use the helper method below.
+			 * 3. Time a loop that runs trials times (trials is the variable above) that:
+			 *     a. Creates a BasicDocument 
+			 *     b. Calls fleshScore on this document
+			 * 4. Print out the time it took to complete the loop in step 3 
+			 *      (on the same line as the first print statement) followed by a tab (\t)
+			 * 5. Time a loop that runs trials times (trials is the variable above) that:
+			 *     a. Creates an EfficientDocument 
+			 *     b. Calls fleshScore on this document
+			 * 6. Print out the time it took to complete the loop in step 5 
+			 *      (on the same line as the first print statement) followed by a newline (\n) 
+			 */  
+			 
+		}
 	
 	}
 	
