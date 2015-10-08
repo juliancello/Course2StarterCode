2b61bd89cc661d8ae57f0880884d49dc979e9657
Christine Alvarado <cjalvarado@eng.ucsd.edu>
2015-10-08 11:06:53 -0700

    added two testers for the Dictionary implementations, and implemented both of these in the starter code.  I will remove the implementations of DictionaryLL and DictionaryBST before launch.  This is just for testing and a question for Adam, which I will email to him.

--- MOOCTextEditor/src/spelling/DictionaryLL.java
+++ MOOCTextEditor/src/spelling/DictionaryLL.java
@@ -10,29 +10,44 @@ public class DictionaryLL implements Dictionary
 {
 
     // TODO: Add member variables and a constructor
-    
+	private LinkedList<String> dict;
+	
+	public DictionaryLL()
+	{
+		dict = new LinkedList<String>();
+	}
 
-    /** Add this word to the dictionary.
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-        // TODO: Implement this method
-        return false;
+        String toAdd = word.toLowerCase();
+        if (dict.contains(toAdd)) return false;
+        dict.add(toAdd);
+        return true;
+    	// TODO: Implement this method
+        //return false;
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
+    	return dict.size();
         // TODO: Implement this method
-        return 0;
+        //return 0;
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        // TODO: Implement this method
-        return false;
+    	String toCheck = s.toLowerCase();
+    	return dict.contains(toCheck);
+    	
+        //TODO: Implement this method
+        //return false;
     }
+
     
 }
