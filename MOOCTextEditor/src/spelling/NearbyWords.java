65a0bc969b32bc56ebccf07a9d414939303e3d9f
Leo Porter <leporter@eng.ucsd.edu>
2015-10-13 15:18:04 -0700

    Revised NearbyWords to match writeup.  Cleanup throughout.

--- MOOCTextEditor/src/spelling/NearbyWords.java
+++ MOOCTextEditor/src/spelling/NearbyWords.java
@@ -3,106 +3,125 @@
  */
 package spelling;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+
 
 /**
  * @author UC San Diego Intermediate MOOC team
  *
  */
-public class NearbyWords implements SpellingSuggest{
-    
-    Dictionary dict;
-   
-   public NearbyWords (Dictionary dict) 
-   {
-      this.dict = dict;
-   }
-   
-   /** Return the list of Strings that are one modification away
-    * from the input string.  
-    * @param s
-    * @param wordsOnly controls whether to return only words or any String
-    * @return
-    */
-   public List<String> distanceOne(String s, boolean wordsOnly ) 
-   {
-	   // TODO: Implement this method
-	   return null;
-   }
+public class NearbyWords implements SpellingSuggest {
+	// THRESHOLD to determine how many words to look through when looking
+	// for spelling suggestions (stops prohibitively long searching)
+	// For use in the Optional Optimization in Part 2.
+	private static final int THRESHOLD = 1000; 
 
-   public void insertions(String s, List<String> currentList, boolean wordsOnly ) 
-   {
-	   // TODO: Implement this method  
-   }
+	Dictionary dict;
 
-   public void changeChar(String s, List<String> currentList, boolean wordsOnly ) {
-	   // TODO: Implement this method
-   }
-   public void deletions(String s, List<String> currentList, boolean wordsOnly ) 
-   {
-	   // TODO: Implement this method
-   }
+	public NearbyWords (Dictionary dict) 
+	{
+		this.dict = dict;
+	}
 
-   @Override
-   public List<String> suggestions(String word, int numSuggestions) {
-	   // remove when ready to test working code below
-	   return new LinkedList<String>();
-	   /*
-	   
-	   // this method creates and calls your NearbyWords class for
-	   // words nearby
-	   List<String> queue = new LinkedList<String>();
-	   HashSet<String> visited = new HashSet<String>();
-	   List<String> retList = new LinkedList<String>();
+	/** Return the list of Strings that are one modification away
+	 * from the input string.  
+	 * @param s The original String
+	 * @param wordsOnly controls whether to return only words or any String
+	 * @return list of Strings which are nearby the original string
+	 */
+	public List<String> distanceOne(String s, boolean wordsOnly )  {
+		   List<String> retList = new ArrayList<String>();
+		   insertions(s, retList, wordsOnly);
+		   changeChar(s, retList, wordsOnly);
+		   deletions(s, retList, wordsOnly);
+		   return retList;
+	}
 
-	   // insert first node
-	   queue.add(word);
-	   visited.add(word);
+	
+	/** Add to the currentList Strings that are one character mutation away
+	 * from the input string.  
+	 * @param s The original String
+	 * @param currentList is the list of words to append modified words 
+	 * @param wordsOnly controls whether to return only words or any String
+	 * @return
+	 */
+	public void changeChar(String s, List<String> currentList, boolean wordsOnly) {
+		// for each letter in the s and for all possible replacement characters
+		for(int index = 0; index < s.length(); index++){
+			for(int charCode = (int)'a'; charCode <= (int)'z'; charCode++) {
+				// use StringBuffer for an easy interface to permuting the 
+				// letters in the String
+				StringBuffer sb = new StringBuffer(s);
+				sb.setCharAt(index, (char)charCode);
 
-	   // If you want to add the original word before starting, uncomment
-	   // the line below
-	   // retList.add(curr);
+				// if the item isn't in the list, isn't the original string, and
+				// (if wordsOnly is true) is a real word, add to the list
+				if(!currentList.contains(sb.toString()) && 
+						(!wordsOnly||dict.isWord(sb.toString())) &&
+						!s.equals(sb.toString())) {
+					currentList.add(sb.toString());
+				}
+			}
+		}
+	}
+	
+	/** Add to the currentList Strings that are one character insertion away
+	 * from the input string.  
+	 * @param s The original String
+	 * @param currentList is the list of words to append modified words 
+	 * @param wordsOnly controls whether to return only words or any String
+	 * @return
+	 */
+	public void insertions(String s, List<String> currentList, boolean wordsOnly ) {
+		// TODO: Implement this method  
+	}
 
-	   // perform BFS while dynamically building the tree
-	   // pop from the queue, get all String permutations 1 away
-	   // if not already visited, add those Strings to the queue
-	   // if the String is a word in the dictionary, add it to the return list
-	   int count = 0;  // only used for debugging
-	   while(!queue.isEmpty() && retList.size() < numSuggestions) {
-		     // Uncomment below for DEBUGGING SUPPORT, beware - large output
-			 // System.out.println("****** ITERATION "+count+"*********");
-		     // System.out.println(queue);
-		    
-		   // remove head
-		   String curr = queue.remove(0);
-		   // find neighboring Strings (not necessarily words)
-		   List<String> neighbors = distanceOne(curr, false);
-		   for(String n : neighbors) {
-			   if(!visited.contains(n)) {
-				   // mark visited
-				   visited.add(n);
-				   // add to the exploration queue
-				   queue.add(n);
-				   // if a word (and we need more suggestions), add to suggestion list
-				   if(retList.size() < numSuggestions && dict.isWord(n)) {
-					   retList.add(n);
-				   }
-			   }
-		   }
-		   count++;
-	   }
-	   return retList;
-	   */
-   }	
+	/** Add to the currentList Strings that are one character deletion away
+	 * from the input string.  
+	 * @param s The original String
+	 * @param currentList is the list of words to append modified words 
+	 * @param wordsOnly controls whether to return only words or any String
+	 * @return
+	 */
+	public void deletions(String s, List<String> currentList, boolean wordsOnly ) {
+		// TODO: Implement this method
+	}
+
+	/** Add to the currentList Strings that are one character deletion away
+	 * from the input string.  
+	 * @param word The misspelled word
+	 * @param numSuggestions is the maximum number of suggestions to return 
+	 * @return the list of spelling suggestions
+	 */
+	@Override
+	public List<String> suggestions(String word, int numSuggestions) {
+
+		// initial variables
+		List<String> queue = new LinkedList<String>();     // String to explore
+		HashSet<String> visited = new HashSet<String>();   // to avoid exploring the same  
+														   // string multiple times
+		List<String> retList = new LinkedList<String>();   // words to return
+		 
+		
+		// insert first node
+		queue.add(word);
+		visited.add(word);
+					
+		// TODO: Implement the remainder of this method, see assignment for algorithm
+		
+		return retList;
+
+	}	
 
    public static void main(String[] args) {
-	   /* testing code
+	   /* basic testing code to get started
 	   String word = "i";
-	   NearbyWords w = new NearbyWords(new DictionaryHashSet("data/dict.txt"));
+	   NearbyWords w = new NearbyWords(new DictionaryHashSet("Course2/data/dict.txt"));
 	   List<String> l = w.distanceOne(word, true);
-	   System.out.println("One away Strings for for \""+word+"\" are:");
+	   System.out.println("One away word Strings for for \""+word+"\" are:");
 	   System.out.println(l+"\n");
 
 	   word = "tailo";
